2f1a110563baf03c3e9c9b80bd03f0418cce924f
Miguel Santacruz <miguel.santacruz@mths.ca>
2022-02-22 05:11:36 +0000

    Hello, world!

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Miguel Santacruz All rights reserved
+//
+// Created by: Miguel Santacruz
+// Created on: Feb 2022
+// This is the "Hello, world!" program, with proper style
+
+#include <iostream>
+
+int main() {
+std::cout << "Hello, world!";
+}
